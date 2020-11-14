9f0d40fab1700245ddecca5c67db46e807bdf6f7
Clayton Watts <cletusw@gmail.com>
2020-11-14 00:02:43 -0700

    Home screen

--- java/src/main/java/LocalizationApp.java
+++ java/src/main/java/LocalizationApp.java
@@ -1,5 +1,10 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LocalizationApp extends Application {
 
@@ -8,8 +13,10 @@ public class LocalizationApp extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello, World!");
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
+        primaryStage.setTitle("Home");
+        primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 }
