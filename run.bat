8048bb0e9755ca3cb589d3252ae49106b9573073
Clayton Watts <cletusw@gmail.com>
2020-11-19 21:10:58 -0700

    run.bat checks for Java and runs first_time_setup.bat if necessary

--- run.bat
+++ run.bat
@@ -1,7 +1,16 @@
 @echo off
 setlocal
-set JAVA_HOME="%~dp0\jdk-15.0.1+9
+
+set JAVA_HOME="%~dp0\jdk-15.0.1+9"
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+
+if exist "%JAVA_EXE%" goto execute
+
+call first_time_setup.bat
+
+:execute
 cd java
 call gradlew.bat run
+
 endlocal
 pause
