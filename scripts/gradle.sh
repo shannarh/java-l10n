437f0de9d0cb4fd9cf76adebeae76425e8d0c645
Clayton Watts <cletusw@gmail.com>
2020-12-04 23:17:58 -0700

    Add Mac support

--- scripts/gradle.sh
+++ scripts/gradle.sh
@@ -2,15 +2,47 @@
 # cd into the directory where this script is located
 cd "${BASH_SOURCE%/*}/" || exit
 
-# TODO: Error handling
+# TODO: Error handling throughout
 
-# TODO: Support Mac
-sdk_filename="OpenJDK15U-jdk_x64_linux_hotspot_15.0.1_9.tar.gz"
-sdk_url="https://github.com/AdoptOpenJDK/openjdk15-binaries/releases/download/jdk-15.0.1%2B9/${sdk_filename}"
+function detect_os() {
+  linux=false
+  darwin=false
+
+  local uname_value
+  uname_value=$(uname)
+  case "${uname_value}" in
+    Linux* )
+      echo "Linux detected"
+      linux=true
+      ;;
+    Darwin* )
+      echo "Mac OS detected"
+      darwin=true
+      ;;
+    * )
+      echo "!!!! Unsupported OS: ${uname_value} !!!!"
+      echo "Falling back to Linux, but it's unlikely to work..."
+      linux=true
+      ;;
+  esac
+}
 
 function install_jdk() {
   echo "This may take several minutes depending on your internet connection..."
   echo
+
+  local sdk_filename
+  if [[ "${linux}" == "true" ]]; then
+    sdk_filename="OpenJDK15U-jdk_x64_linux_hotspot_15.0.1_9.tar.gz"
+  elif [[ "${darwin}" == "true" ]]; then
+    sdk_filename="OpenJDK15U-jdk_x64_mac_hotspot_15.0.1_9.tar.gz"
+  else
+    echo "ERROR: Invalid OS detected"
+    exit 1
+  fi
+
+  local sdk_url
+  sdk_url="https://github.com/AdoptOpenJDK/openjdk15-binaries/releases/download/jdk-15.0.1%2B9/${sdk_filename}"
 
   local tmp_dir
   # 1st one is for Linux & Mac OS >=10.11, 2nd for Mac OS <10.11
@@ -34,17 +66,25 @@ function install_jdk() {
 }
 
 function main() {
-  if [ ! -f "${JAVA_HOME}/bin/javac" ]
-  then
+  detect_os
+
+  if [ ! -f "${JAVA_HOME}/bin/javac" ]; then
     # echo "No JDK found in JAVA_HOME"
 
     # Provide a value for GRADLE_USER_HOME if not set
     : ${GRADLE_USER_HOME:="${HOME}/.gradle"}
 
-    JAVA_HOME="${GRADLE_USER_HOME}/jdks/jdk-15.0.1+9"
 
-    if [ ! -f "${JAVA_HOME}/bin/javac" ]
-    then
+    if [[ "${linux}" == "true" ]]; then
+      JAVA_HOME="${GRADLE_USER_HOME}/jdks/jdk-15.0.1+9"
+    elif [[ "${darwin}" == "true" ]]; then
+      JAVA_HOME="${GRADLE_USER_HOME}/jdks/jdk-15.0.1+9/Contents/Home"
+    else
+      echo "ERROR: Invalid OS detected"
+      exit 1
+    fi
+
+    if [ ! -f "${JAVA_HOME}/bin/javac" ]; then
       # echo "No JDK found in ${GRADLE_USER_HOME}"
 
       install_jdk
